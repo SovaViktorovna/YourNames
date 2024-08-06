e7bea27acf62b7b961889a3b11ac9eeb4847a4d2
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-08-06 12:26:02 +0400

    Initial Commit

---
+++ YourNames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourNames
+//
+//  Created by Виктория Демченко on 06.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
